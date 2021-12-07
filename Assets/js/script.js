b60289c845b60a259a70be10cbebc698c222fd0a
josevidmal <92562836+josevidmal@users.noreply.github.com>
2021-12-06 19:28:50 -0600

    Added for loop to create timeblocks elements with jQuery

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -5,3 +5,47 @@ $("#currentDay").text(currentDateTime.format("MMMM Do YYYY, h:mm:ss a"));
 
 setInterval(dateTime, 1000);
 
+var containerEl = $(".container");
+containerEl.css({"display": "flex", "flex-wrap": "wrap"});
+
+var hours = [
+    "9AM", 
+    "10AM",
+    "11AM",
+    "12PM",
+    "1PM",
+    "2PM",
+    "3PM",
+    "4PM",
+    "5PM"
+];
+
+for (var i = 0; i < hours.length; i++) {
+    var timeblock = $("<div>");
+    timeblock.addClass("input-group time-block row");
+    timeblock.attr("data-time", [i]);
+    containerEl.append(timeblock);
+
+    var time = $("<span>");
+    time.addClass("description hour");
+    time.text(hours[i]);
+    time.css({"flex": "0 0 10%"});
+    timeblock.append(time);
+
+    var textArea = $("<textarea>");
+    textArea.addClass("form-control");
+    textArea.attr({"id": "text-" + hours[i], "aria-label": "With textarea"});
+    textArea.css({"flex": "0 0 80%"});
+    timeblock.append(textArea);
+
+    var saveBtn = $("<button>");
+    saveBtn.addClass("btn btn-info saveBtn");
+    saveBtn.attr({"id": "btn-" + hours[i], "type": "button"});
+    saveBtn.css({"flex": "0 0 10%"});
+    timeblock.append(saveBtn);
+
+    var btnIcon = $("<i>");
+    btnIcon.addClass("fas fa-save");
+    saveBtn.append(btnIcon);
+}
+
