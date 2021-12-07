58688ec317cccc5a949b699878254215690d107d
josevidmal <92562836+josevidmal@users.noreply.github.com>
2021-12-07 16:19:02 -0600

    Added for loop with moment.js and jQuery to set color code for past, present and future timeblocks

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,7 +1,7 @@
-// function to set date and time with moment.js
+// Function to set date and time with moment.js
 function dateTime() {
-var currentDateTime = moment();
-$("#currentDay").text(currentDateTime.format("MMMM Do YYYY, h:mm:ss a"));
+    var currentDateTime = moment().format("MMMM Do YYYY, h:mm:ss a");
+    $("#currentDay").text(currentDateTime);
 }
 
 setInterval(dateTime, 1000);
@@ -9,7 +9,7 @@ setInterval(dateTime, 1000);
 var containerEl = $(".container");
 containerEl.css({"display": "flex", "flex-wrap": "wrap"});
 
-// hours array to use in for loop
+// Hours array to use in for loop
 var hours = [
     "9AM", 
     "10AM",
@@ -22,7 +22,7 @@ var hours = [
     "5PM"
 ];
 
-// for loop to create html elements for timeblocks using jQuery
+// For loop to create html elements for timeblocks using jQuery
 for (var i = 0; i < hours.length; i++) {
     var timeblock = $("<div>");
     timeblock.addClass("input-group time-block row");
@@ -67,7 +67,7 @@ saveBtn9AM.on("click", function() {
 });
 
 var saveBtn10AM = $("#btn-10AM");
-var text10AM = $("#text-10AM")
+var text10AM = $("#text-10AM");
 
 text10AM.val(localStorage.getItem("10AM"));
 
@@ -79,7 +79,7 @@ saveBtn10AM.on("click", function() {
 });
 
 var saveBtn11AM = $("#btn-11AM");
-var text11AM = $("#text-11AM")
+var text11AM = $("#text-11AM");
 
 text11AM.val(localStorage.getItem("11AM"));
 
@@ -91,7 +91,7 @@ saveBtn11AM.on("click", function() {
 });
 
 var saveBtn12PM = $("#btn-12PM");
-var text12PM = $("#text-12PM")
+var text12PM = $("#text-12PM");
 
 text12PM.val(localStorage.getItem("12PM"));
 
@@ -103,7 +103,7 @@ saveBtn12PM.on("click", function() {
 });
 
 var saveBtn1PM = $("#btn-1PM");
-var text1PM = $("#text-1PM")
+var text1PM = $("#text-1PM");
 
 text1PM.val(localStorage.getItem("1PM"));
 
@@ -115,7 +115,7 @@ saveBtn1PM.on("click", function() {
 });
 
 var saveBtn2PM = $("#btn-2PM");
-var text2PM = $("#text-2PM")
+var text2PM = $("#text-2PM");
 
 text2PM.val(localStorage.getItem("2PM"));
 
@@ -127,7 +127,7 @@ saveBtn2PM.on("click", function() {
 });
 
 var saveBtn3PM = $("#btn-3PM");
-var text3PM = $("#text-3PM")
+var text3PM = $("#text-3PM");
 
 text3PM.val(localStorage.getItem("3PM"));
 
@@ -139,7 +139,7 @@ saveBtn3PM.on("click", function() {
 });
 
 var saveBtn4PM = $("#btn-4PM");
-var text4PM = $("#text-4PM")
+var text4PM = $("#text-4PM");
 
 text4PM.val(localStorage.getItem("4PM"));
 
@@ -151,7 +151,7 @@ saveBtn4PM.on("click", function() {
 });
 
 var saveBtn5PM = $("#btn-5PM");
-var text5PM = $("#text-5PM")
+var text5PM = $("#text-5PM");
 
 text5PM.val(localStorage.getItem("5PM"));
 
@@ -161,3 +161,31 @@ saveBtn5PM.on("click", function() {
 
     localStorage.setItem("5PM", newTask5PM);
 });
+
+// Times array to use in for loop 
+    var timesArr = [
+        "09:00:00 am",
+        "10:00:00 am",
+        "11:00:00 am",
+        "12:00:00 pm",
+        "01:00:00 pm",
+        "02:00:00 pm",
+        "03:00:00 pm",
+        "04:00:00 pm",
+        "05:00:00 pm"
+    ];
+
+// For loop with moments.js and jQuery to set color code for past, present and future timeblocks
+    for (var i = 0; i < timesArr.length; i++) {
+        var currentTime = moment();
+        var times = moment(timesArr[i], "hh:mm:ss a");
+        var text = $("#text-" + hours[i]);
+
+        if (currentTime.isAfter(times)) {
+            text.addClass("past");
+        } if ((currentTime.isSameOrAfter(times)) && (currentTime.isBefore(moment(timesArr[i + 1], "hh:mm:ss a")))) {
+            text.addClass("present");
+        } if (currentTime.isBefore(times)) {
+            text.addClass("future");
+        }
+    };
